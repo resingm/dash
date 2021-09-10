d6b8de7a012fbe1d03f895202497858b0f75b676
Max Resing <max.resing@protonmail.com>
2021-09-10 15:46:08 +0200

    Added state management

---
+++ collector/collector/state.py
@@ -0,0 +1,49 @@
+import json
+from typing import Union
+from utils import wrap_component
+
+from pyredis import RedisConnection
+
+
+class StateManager:
+    def __init__(self):
+        self._state = {}
+
+    @property
+    def state(self):
+        # Ensure an order by iterating over keys
+        return {
+            "data": [self._state[k] for k in self._state.keys()],
+        }
+
+    def update(self, key: str, val: Union[str, dict]):
+        self._state[key] = val
+
+
+def update(
+    mgr: StateManager,
+    card_type: str,
+    id: str,
+    title: str,
+    f: callable,
+    subtitle: str = None,
+    *args,
+    **kwargs,
+):
+    val = f(*args, **kwargs)
+    comp = wrap_component(
+        id=id,
+        title=title,
+        subtitle=subtitle,
+        card_type=card_type,
+        attributes=val,
+    )
+    mgr.update(id, comp)
+
+
+def upload(mgr: StateManager, rc: RedisConnection, topic: str):
+    try:
+        # val = json.dumps(mgr.state)
+        rc.set(topic, mgr.state)
+    except Exception as e:
+        raise e
